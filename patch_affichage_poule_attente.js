21a7ad26e74d68c36bb35fad71e29d8d66ee2377
mmaunier <mmaunier2001@yahoo.fr>
2026-03-22 17:41:40 +0100

    chore: clean up scripts

--- patch_affichage_poule_attente.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const pathSync = 'js/pages/TournoiPage.js';
-let contentSync = fs.readFileSync(pathSync, 'utf8');
-
-// Need to figure out where joueursAttente is populated
--- test_attente_patch.js
+++
@@ -1,2 +0,0 @@
-const fs = require('fs')
-console.log(fs.readFileSync('js/pages/TournoiPage.js', 'utf8').includes('// Inject poule name'))
